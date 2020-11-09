cc3366acc8cbb3bf9c36c33449f81b50bc904b55
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-11-09 11:44:37 +0100

    add functions and reduce unnecessary code

--- js/script.js
+++ js/script.js
@@ -1,51 +1,36 @@
-let computerMove;
-let playerMove;
+const getMoveName = id => {
+  if (id == 1) {
+    return 'Kamień';
+  } else if (id == 2) {
+    return 'Papier';
+  } else if (id == 3) {
+    return 'Nożyce';
+  }
+}
 const randomNumber = Math.floor(Math.random() * 3 + 1);
 const playerInput = prompt(`Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.`);
-console.log(`gracz wpisał ${playerInput}`);
 
-if (randomNumber == 1) {
-  computerMove = 'Kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'Papier';
-} else if (randomNumber == 3) {
-  computerMove = 'Nożyce';
-}
-
-console.log(`Ruch komputera to: ${computerMove}`);
-
-if (playerInput == '1') {
-  playerMove = 'Kamień';
-} else if (playerInput == '2') {
-  playerMove = 'Papier';
-} else if (playerInput == '3') {
-  playerMove = 'Nożyce';
-}
-
-if (computerMove == 'Kamień' && playerMove == 'Papier') {
-  printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-} else if (computerMove == 'Kamień' && playerMove == 'Nożyce') {
-  printMessage(`No niestety przegrałeś :(`);
-} else if (computerMove == 'Kamień' && playerMove == 'Kamień') {
-  printMessage(`Mamy remis`);
-}
-
-if (computerMove == 'Papier' && playerMove == 'Kamień') {
-  printMessage(`No niestety przegrałeś :(`);
-} else if (computerMove == 'Papier' && playerMove == 'Papier') {
-  printMessage(`Mamy remis`);
-} else if (computerMove == 'Papier' && playerMove == 'Nożyce') {
-  printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-}
-
-if (computerMove == 'Nożyce' && playerMove == 'Kamień') {
-  printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-} else if (computerMove == 'Nożyce' && playerMove == 'Papier') {
-  printMessage(`No niestety przegrałeś :(`);
-} else if (computerMove == 'Nożyce' && playerMove == 'Nożyce') {
-  printMessage(`Mamy remis`);
-}
+let computerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
 
-printMessage(`Ruch komputera to: ${computerMove}`);
-printMessage(`Twój ruch to: ${playerMove}`);
+const displayResult = (argCPU, argPlayer) => {
+  printMessage(`Komputer zagrał ${argCPU}, a Ty zagrałeś ${argPlayer}`);
+
+  if (argCPU === argPlayer) {
+    printMessage(`Mamy remis`);
+  } else if (argCPU == `Kamień` && argPlayer == `Papier`) {
+    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+  } else if (argCPU == `Kamień` && argPlayer == `Nożyce`) {
+    printMessage(`No niestety przegrałeś :(`);
+  } else if (argCPU == `Papier` && argPlayer == `Kamień`) {
+    printMessage(`No niestety przegrałeś :(`);
+  } else if (argCPU == `Papier` && argPlayer == `Nożyce`) {
+    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+  } else if (argCPU == `Nożyce` && argPlayer == `Kamień`) {
+    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+  } else if (argCPU == `Nożyce` && argPlayer == `Papier`) {
+    printMessage(`No niestety przegrałeś :(`);
+  }
+};
+displayResult(computerMove, playerMove);
