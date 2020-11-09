3ed0090f9fd68fb3753083403b79c3a5cc60d1b0
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-11-09 12:45:18 +0100

    buttons added and new functionality

--- js/script.js
+++ js/script.js
@@ -1,36 +1,49 @@
-const getMoveName = id => {
-  if (id == 1) {
-    return 'Kamień';
-  } else if (id == 2) {
-    return 'Papier';
-  } else if (id == 3) {
-    return 'Nożyce';
+const playGame = (playerInput) => {
+  clearMessages();
+  const getMoveName = id => {
+    if (id == 1) {
+      return 'Kamień';
+    } else if (id == 2) {
+      return 'Papier';
+    } else if (id == 3) {
+      return 'Nożyce';
+    }
   }
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  // const playerInput = prompt(`Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.`);
+
+  let computerMove = getMoveName(randomNumber);
+  let playerMove = getMoveName(playerInput);
+
+
+  const displayResult = (argCPU, argPlayer) => {
+    printMessage(`Komputer zagrał ${argCPU}, a Ty zagrałeś ${argPlayer}`);
+
+    if (argCPU === argPlayer) {
+      printMessage(`Mamy remis`);
+    } else if (argCPU == `Kamień` && argPlayer == `Papier`) {
+      printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+    } else if (argCPU == `Kamień` && argPlayer == `Nożyce`) {
+      printMessage(`No niestety przegrałeś :(`);
+    } else if (argCPU == `Papier` && argPlayer == `Kamień`) {
+      printMessage(`No niestety przegrałeś :(`);
+    } else if (argCPU == `Papier` && argPlayer == `Nożyce`) {
+      printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+    } else if (argCPU == `Nożyce` && argPlayer == `Kamień`) {
+      printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
+    } else if (argCPU == `Nożyce` && argPlayer == `Papier`) {
+      printMessage(`No niestety przegrałeś :(`);
+    }
+  };
+  displayResult(computerMove, playerMove);
 }
-const randomNumber = Math.floor(Math.random() * 3 + 1);
-const playerInput = prompt(`Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.`);
 
-let computerMove = getMoveName(randomNumber);
-let playerMove = getMoveName(playerInput);
-
-
-const displayResult = (argCPU, argPlayer) => {
-  printMessage(`Komputer zagrał ${argCPU}, a Ty zagrałeś ${argPlayer}`);
-
-  if (argCPU === argPlayer) {
-    printMessage(`Mamy remis`);
-  } else if (argCPU == `Kamień` && argPlayer == `Papier`) {
-    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-  } else if (argCPU == `Kamień` && argPlayer == `Nożyce`) {
-    printMessage(`No niestety przegrałeś :(`);
-  } else if (argCPU == `Papier` && argPlayer == `Kamień`) {
-    printMessage(`No niestety przegrałeś :(`);
-  } else if (argCPU == `Papier` && argPlayer == `Nożyce`) {
-    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-  } else if (argCPU == `Nożyce` && argPlayer == `Kamień`) {
-    printMessage(`Brawo!!! Ty wygrywasz !!! :D`);
-  } else if (argCPU == `Nożyce` && argPlayer == `Papier`) {
-    printMessage(`No niestety przegrałeś :(`);
-  }
-};
-displayResult(computerMove, playerMove);
+document.getElementById('play-rock').addEventListener('click', function () {
+  playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function () {
+  playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function () {
+  playGame(3);
+});
